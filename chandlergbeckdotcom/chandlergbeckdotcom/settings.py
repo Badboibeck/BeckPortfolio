5d4b07912fd91a6ad50aed6815cd7addf9a466ec
babybirdbeck <cgenebeck96@gmail.com>
2021-11-08 11:56:33 -0700

    hotfix

--- chandlergbeckdotcom/chandlergbeckdotcom/settings.py
+++ chandlergbeckdotcom/chandlergbeckdotcom/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@*bhn+#t)yji+s$=6si+=&a58hhwn5!ybum9h=-bmpq9dr=)m-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -121,10 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join('/home', 'ubuntu', 'project', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
